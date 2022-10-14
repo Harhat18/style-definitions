e325c4f846bcd36890a5de47ef986dff11155230
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-14 20:28:23 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <div style={{ color: "red" }}>Harun Hatiboğlu</div>
+        <div style={{ color: "red", backgroundColor: "white", paddingTop: 50 }}>
+          Harun Hatiboğlu
+        </div>
       </header>
     </div>
   );
